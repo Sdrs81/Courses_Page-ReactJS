f407db026127ce4d0ccb84a3caed8a6c21bae96c
Sdrs81 <serdardemirtas25@gmail.com>
2023-03-16 20:20:09 +0300

    Course Component has been created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import './App.css';
+import "./App.css";
+import Course from "./Course";
 
 function App() {
   return (
     <div className="App">
-      Can
+      <Course
+        title="Angular"
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos doloribus
+        asperiores ipsum, nam similique quos, dolores quia vitae, tenetur maiores
+        dolore nisi ducimus eum consequuntur iure recusandae enim. Magnam, eius."
+      />
+      <Course
+        title="Bootstrap 5"
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos doloribus
+        asperiores ipsum, nam similique quos, dolores quia vitae, tenetur maiores
+        dolore nisi ducimus eum consequuntur iure recusandae enim. Magnam, eius."
+      />
+      <Course
+        title="KompleWeb"
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos doloribus
+      asperiores ipsum, nam similique quos, dolores quia vitae, tenetur maiores
+      dolore nisi ducimus eum consequuntur iure recusandae enim. Magnam, eius."
+      />
+      <Course
+        title="Frontend"
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos doloribus
+        asperiores ipsum, nam similique quos, dolores quia vitae, tenetur maiores
+        dolore nisi ducimus eum consequuntur iure recusandae enim. Magnam, eius."
+      />
     </div>
   );
 }
---
+++ src/Course.js
@@ -0,0 +1,10 @@
+function Course({title, description}) {
+  return (
+    <div>
+      <div>{title}</div>
+      <div>{description}</div>
+    </div>
+  );
+}
+
+export default Course;
